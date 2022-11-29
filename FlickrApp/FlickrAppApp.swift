7bec48566e53b0116614a48e7cdf21b41e52090f
Mikhail <mich_p@inbox.ru>
2022-11-29 18:47:28 +0300

    Initial Commit

---
+++ FlickrApp/FlickrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAppApp.swift
+//  FlickrApp
+//
+//  Created by Миша Перевозчиков on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlickrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
